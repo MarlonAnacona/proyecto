55607f3ae151b4cf1b83152f1db48c87e1c47823
MarlonAnacona <ejemplo@email.com>
2022-07-10 13:47:07 -0500

    update scala

--- src/main/scala/paquete/package.scala
+++ src/main/scala/paquete/package.scala
@@ -1,9 +1,7 @@
 package object paquete {
 
 
-class infoaeropuerto(cod: String){
 
-}
   def aeropuertos(cod: String, x: Int, y: Int, gmt: Int ): Unit ={
 
   }
@@ -13,6 +11,27 @@ class infoaeropuerto(cod: String){
   }
 
   def itenerario(a1:String,a2:String): Unit ={
+
+  }
+
+  //Busca los intenierarios de menor tiempo , al menos 2
+  def itinerariosTiempo(a1: String, a2:String ): Unit ={
+
+  }
+
+
+  //El menor cambio de avion de los itenerarios
+  def itinerariosCambios(a1:String,a2:String): Unit ={
+
+  }
+
+  //Itenerarios con menor  el tiempo de vuelo
+  def itenerarioDistancia(a1:String,a2:String): Unit ={
+
+
+  }
+//Optimizacion en horario de salida
+  def itenerariosSalida(a1:String,a2:String,h:String,m:String): Unit ={
 
   }
 
