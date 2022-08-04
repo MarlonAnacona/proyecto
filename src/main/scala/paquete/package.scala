1a1fa8c4716bdc73240034a35d5157aafb31b360
MarlonAnacona <ejemplo@email.com>
2022-08-04 10:54:34 -0500

    Update horas GMT

--- src/main/scala/paquete/package.scala
+++ src/main/scala/paquete/package.scala
@@ -16,29 +16,29 @@ object paquete {
     case class Vuelo(Aln: String, Num: Int, Org: String, HS: Int, MS: Int, Dst: String, HL: Int, ML: Int, Esc: Int) extends aero
 
     val aeropuertos = List(
-      Aeropuerto("ABQ", 195, 275, -8),
-      Aeropuerto("ATL", 470, 280, -6),
-      Aeropuerto("BNA", 430, 240, -7),
-      Aeropuerto("BOS", 590, 100, -6),
-      Aeropuerto("DCA", 540, 180, -6),
-      Aeropuerto("DEN", 215, 205, -8),
-      Aeropuerto("DFW", 310, 305, -7),
-      Aeropuerto("DTW", 445, 140, -6),
-      Aeropuerto("HOU", 330, 355, -7),
-      Aeropuerto("JFK", 565, 130, -6),
-      Aeropuerto("LAX", 55, 270, -9),
-      Aeropuerto("MIA", 535, 390, -6),
-      Aeropuerto("MSP", 340, 115, -7),
-      Aeropuerto("MSY", 405, 345, -7),
-      Aeropuerto("ORD", 410, 155, -7),
-      Aeropuerto("PHL", 550, 155, -6),
-      Aeropuerto("PHX", 120, 290, -8),
-      Aeropuerto("PVD", 595, 122, -6),
-      Aeropuerto("RDU", 530, 230, -6),
-      Aeropuerto("SEA", 55, 45, -9),
-      Aeropuerto("SFO", 10, 190, -9),
-      Aeropuerto("STL", 380, 210, -7),
-      Aeropuerto("TPA", 500, 360, -6)
+      Aeropuerto("ABQ", 195, 275, -800),
+      Aeropuerto("ATL", 470, 280, -600),
+      Aeropuerto("BNA", 430, 240, -700),
+      Aeropuerto("BOS", 590, 100, -600),
+      Aeropuerto("DCA", 540, 180, -600),
+      Aeropuerto("DEN", 215, 205, -800),
+      Aeropuerto("DFW", 310, 305, -700),
+      Aeropuerto("DTW", 445, 140, -600),
+      Aeropuerto("HOU", 330, 355, -700),
+      Aeropuerto("JFK", 565, 130, -600),
+      Aeropuerto("LAX", 55, 270, -900),
+      Aeropuerto("MIA", 535, 390, -600),
+      Aeropuerto("MSP", 340, 115, -700),
+      Aeropuerto("MSY", 405, 345, -700),
+      Aeropuerto("ORD", 410, 155, -700),
+      Aeropuerto("PHL", 550, 155, -600),
+      Aeropuerto("PHX", 120, 290, -800),
+      Aeropuerto("PVD", 595, 122, -600),
+      Aeropuerto("RDU", 530, 230, -600),
+      Aeropuerto("SEA", 55, 45, -900),
+      Aeropuerto("SFO", 10, 190, -900),
+      Aeropuerto("STL", 380, 210, -700),
+      Aeropuerto("TPA", 500, 360, -600)
     )
 
     //________________________________________________________
@@ -78,80 +78,6 @@ object paquete {
       Vuelo("AA", 203, "DFW", 8, 10, "BOS", 14, 7, 0),
       Vuelo("AA", 334, "RDU", 9, 0, "PVD", 10, 40, 0),
       Vuelo("AA", 709, "ATL", 8, 28, "MIA", 10, 15, 0)
-    )
-
-    val vuelosA2 = List(
-      Vuelo("AA", 35, "RDU", 14, 10, "MIA", 16, 21, 0),
-      Vuelo("AA", 179, "ORD", 18, 30, "PHX", 11, 12, 0),
-      Vuelo("AA", 580, "SFO", 12, 25, "ORD", 11, 5, 1),
-      Vuelo("AA", 70, "SFO", 8, 8, "DFW", 13, 31, 0),
-      Vuelo("AA", 835, "DTW", 17, 0, "ORD", 17, 13, 0),
-      Vuelo("AA", 890, "PHX", 1, 40, "ORD", 15, 5, 0),
-      Vuelo("AA", 784, "DFW", 10, 54, "ORD", 12, 56, 0),
-      Vuelo("AA", 796, "DEN", 15, 7, "BNA", 18, 47, 0),
-      Vuelo("AA", 89, "ORD", 18, 30, "LAX", 10, 35, 0),
-      Vuelo("AA", 920, "MIA", 8, 44, "DCA", 11, 6, 0),
-
-      Vuelo("AA", 837, "ATL", 7, 25, "DFW", 13, 5, 0),
-      Vuelo("AA", 728, "DFW", 6, 35, "BOS", 10, 58, 0),
-
-      Vuelo("AA", 203, "DFW", 8, 10, "BOS", 14, 7, 0),
-      Vuelo("AA", 334, "RDU", 9, 0, "PVD", 10, 40, 0),
-      Vuelo("AA", 709, "ATL", 8, 28, "MIA", 10, 15, 0)
-    )
-
-    val vuelosA3 = List(
-      Vuelo("AA", 400, "LAX", 7, 30, "MIA", 15, 30, 0),
-      Vuelo("AA", 978, "MIA", 8, 10, "MSY", 12, 4, 0),
-      Vuelo("AA", 348, "ORD", 6, 15, "DFW", 13, 6, 0),
-      Vuelo("AA", 71, "DFW", 16, 2, "SFO", 17, 43, 0),
-      Vuelo("AA", 860, "BNA", 19, 27, "DTW", 11, 55, 0),
-      Vuelo("AA", 749, "BOS", 18, 17, "BNA", 10, 12, 0),
-      Vuelo("AA", 673, "ATL", 19, 57, "ORD", 10, 45, 0),
-      Vuelo("AA", 926, "MIA", 19, 3, "MSY", 10, 17, 0),
-      Vuelo("AA", 926, "MIA", 19, 3, "HOU", 12, 15, 2),
-      Vuelo("AA", 586, "DFW", 17, 13, "PHL", 11, 13, 0),
-      Vuelo("AA", 637, "PVD", 7, 35, "RDU", 12, 5, 0),
-      Vuelo("AA", 329, "DFW", 14, 27, "ABQ", 15, 15, 0),
-      Vuelo("AA", 568, "DFW", 6, 56, "DCA", 10, 33, 0),
-      Vuelo("AA", 530, "SFO", 12, 22, "BOS", 12, 54, 1),
-      Vuelo("AA", 379, "BOS", 6, 15, "ORD", 15, 4, 0)
-    )
-
-    val vuelosA4 = List(
-      Vuelo("AA", 27, "MIA", 8, 0, "SEA", 11, 27, 0),
-      Vuelo("AA", 1, "JFK", 9, 0, "LAX", 11, 39, 0),
-      Vuelo("AA", 272, "HOU", 7, 25, "DFW", 13, 7, 0),
-      Vuelo("AA", 227, "DFW", 9, 24, "LAX", 10, 17, 0),
-      Vuelo("AA", 2, "LAX", 8, 30, "JFK", 16, 46, 0),
-      Vuelo("AA", 523, "MIA", 7, 45, "DFW", 15, 1, 0),
-      Vuelo("AA", 64, "SFO", 8, 0, "JFK", 16, 23, 0),
-      Vuelo("AA", 181, "ORD", 9, 15, "LAX", 11, 10, 0),
-      Vuelo("AA", 719, "TPA", 13, 20, "DFW", 14, 56, 0),
-      Vuelo("AA", 127, "ORD", 18, 25, "SEA", 10, 58, 0),
-      Vuelo("AA", 407, "ORD", 9, 10, "PHX", 11, 53, 0),
-      Vuelo("AA", 606, "LAX", 7, 0, "DFW", 11, 57, 0),
-      Vuelo("AA", 897, "DCA", 17, 54, "MIA", 10, 31, 0),
-      Vuelo("AA", 566, "SFO", 7, 0, "BOS", 17, 19, 1),
-      Vuelo("AA", 315, "ORD", 17, 30, "BNA", 18, 54, 0)
-    )
-
-    val vuelosA5 = List(
-      Vuelo("AA", 856, "DFW", 14, 30, "HOU", 15, 39, 0),
-      Vuelo("AA", 846, "DFW", 17, 16, "DCA", 11, 4, 0),
-      Vuelo("AA", 745, "MIA", 16, 48, "DFW", 19, 5, 0),
-      Vuelo("AA", 278, "ORD", 16, 30, "BOS", 19, 47, 0),
-      Vuelo("AA", 483, "TPA", 17, 20, "RDU", 19, 1, 0),
-      Vuelo("AA", 705, "DCA", 12, 45, "ORD", 13, 52, 0),
-      Vuelo("AA", 212, "DFW", 17, 21, "DTW", 12, 40, 1),
-      Vuelo("AA", 216, "DFW", 17, 15, "STL", 19, 4, 0),
-      Vuelo("AA", 692, "MIA", 14, 5, "PHL", 16, 50, 0),
-      Vuelo("AA", 705, "DCA", 12, 45, "DEN", 16, 30, 1),
-      Vuelo("AA", 281, "ORD", 11, 15, "LAX", 15, 37, 1),
-      Vuelo("AA", 501, "STL", 13, 5, "DFW", 14, 51, 0),
-      Vuelo("AA", 721, "DCA", 11, 59, "RDU", 13, 9, 0),
-      Vuelo("AA", 201, "PHL", 7, 24, "DFW", 10, 0, 0),
-      Vuelo("AA", 59, "JFK", 8, 0, "SFO", 11, 33, 0)
     )
 
     type itn = List[Vuelo]
@@ -263,7 +189,7 @@ object paquete {
     def horaSalidaAuxiliar(ls: (Vuelo)): Int = {
 
 
-      val num = for (a <- aeropuertos if (ls.Org == a.Cod)) yield (a.GMT) + ls.HS
+      val num = for (a <- aeropuertos if (ls.Org == a.Cod)) yield (a.GMT/100) + ls.HS
       var num2 = num.apply(0);
 
       if (num2 > 24) {
@@ -281,7 +207,7 @@ object paquete {
 
     def horaLlegadaAuxiliar(ls: (Vuelo)): Int = {
 
-      val num = for (a <- aeropuertos if (ls.Dst == a.Cod)) yield a.GMT + ls.HL
+      val num = for (a <- aeropuertos if (ls.Dst == a.Cod)) yield (a.GMT/100) + ls.HL
       var num2 = num.apply(0);
 
       if (num2 > 24) {
@@ -349,7 +275,7 @@ object paquete {
 
     def horaLlegadaAuxiliarNecesaria(ls: (Vuelo), h: Int): Int = {
 
-      val num = for (a <- aeropuertos if (ls.Dst == a.Cod)) yield a.GMT + h
+      val num = for (a <- aeropuertos if (ls.Dst == a.Cod)) yield (a.GMT/100) + h
       var num2 = num.apply(0);
 
       if (num2 > 24) {
@@ -534,7 +460,7 @@ object paquete {
     def horaSalidaAuxiliarPar(ls: (Vuelo)): Int = {
 
 
-      val num = for (a <- aeropuertos if (ls.Org == a.Cod)) yield (a.GMT) + ls.HS
+      val num = for (a <- aeropuertos if (ls.Org == a.Cod)) yield (a.GMT/100) + ls.HS
       var num2 = num.apply(0);
 
       if (num2 > 24) {
@@ -552,7 +478,7 @@ object paquete {
 
     def horaLlegadaAuxiliarPar(ls: (Vuelo)): Int = {
 
-      val num = for (a <- aeropuertos if (ls.Dst == a.Cod)) yield a.GMT + ls.HL
+      val num = for (a <- aeropuertos if (ls.Dst == a.Cod)) yield (a.GMT/100) + ls.HL
 
 
       var num2 = num.apply(0);
@@ -615,14 +541,14 @@ object paquete {
         }
       }
 
-      itenerarioPar(a1, a2).sortBy(r => tiempoAux(r)).par.toList
+      itenerarioPar(a1, a2).sortBy(r => tiempoAux(r)).par.toList.take(3)
 
     }
 
 
     def horaLlegadaAuxiliarNecesariaPar(ls: (Vuelo), h: Int): Int = {
 
-      val num = for (a <- aeropuertos if (ls.Dst == a.Cod)) yield a.GMT + h
+      val num = for (a <- aeropuertos if (ls.Dst == a.Cod)) yield (a.GMT/100) + h
       var num2 = num.apply(0);
 
       if (num2 > 24) {
