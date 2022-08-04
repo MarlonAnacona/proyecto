785248955d904abe860d3ddab43f358a328993fb
Mayra <mayra.alejandra.sanchez@correounivalle.edu.co>
2022-08-04 15:45:01 -0500

    arrglo de errores

--- src/main/scala/paquete/PhotoCanvas.scala
+++ src/main/scala/paquete/PhotoCanvas.scala
@@ -1,10 +1,7 @@
 package paquete
 
 
-import java.awt.Graphics
-import java.awt.image.BufferedImage
 import javax.swing.{JComponent, JTextArea}
-import paquete.vuelosItinerarios
 
 class PhotoCanvas extends JComponent {
 
@@ -15,20 +12,20 @@ class PhotoCanvas extends JComponent {
     filterName match {
       case "Paralelo" =>
         accion match {
-          case "Itinerarios" => vuelosItinerarios.itenerarioPar(a1, a2)
+          case "Itinerarios" => vuelosItinerarios.itinerarioPar(a1, a2)
           case "Itinerarios Tiempo Vuelo" => vuelosItinerarios.itinerariosTiempoPar(a1, a2)
-          case "Itinerarios Distancia" => vuelosItinerarios.itenerarioDistanciaPar(a1, a2)
+          case "Itinerarios Distancia" => vuelosItinerarios.itinerarioDistanciaPar(a1, a2)
           case "itinerarios escala" => vuelosItinerarios.itinerariosCambiosPar(a1, a2)
-          case "Itinerario Salida" => vuelosItinerarios.itenerariosSalidaPar(a1, a2, hora, min)
+          case "Itinerario Salida" => vuelosItinerarios.itinerariosSalidaPar(a1, a2, hora, min)
         }
 
       case "Secuencial" =>
         accion match {
-          case "Itinerarios" => vuelosItinerarios.itenerario(a1, a2)
+          case "Itinerarios" => vuelosItinerarios.itinerario(a1, a2)
           case "Itinerarios Tiempo Vuelo" => vuelosItinerarios.itinerariosTiempo(a1, a2)
-          case "Itinerarios Distancia" => vuelosItinerarios.itenerarioDistancia(a1, a2)
+          case "Itinerarios Distancia" => vuelosItinerarios.itinerarioDistancia(a1, a2)
           case "itinerarios escala" => vuelosItinerarios.itinerariosCambios(a1, a2)
-          case "Itinerario Salida" => vuelosItinerarios.itenerariosSalida(a1, a2, hora, min)
+          case "Itinerario Salida" => vuelosItinerarios.itinerariosSalida(a1, a2, hora, min)
         }
       case "" =>
     }
@@ -39,20 +36,20 @@ class PhotoCanvas extends JComponent {
     filterName match {
       case "Paralelo" =>
         accion match {
-          case "Itinerarios" => campo.setText("" + vuelosItinerarios.itenerarioPar(a1, a2))
+          case "Itinerarios" => campo.setText("" + vuelosItinerarios.itinerarioPar(a1, a2))
           case "Itinerarios Tiempo Vuelo" => campo.setText("" + vuelosItinerarios.itinerariosTiempoPar(a1, a2))
-          case "Itinerarios Distancia" => campo.setText("" + vuelosItinerarios.itenerarioDistanciaPar(a1, a2))
+          case "Itinerarios Distancia" => campo.setText("" + vuelosItinerarios.itinerarioDistanciaPar(a1, a2))
           case "itinerarios escala" => campo.setText("" + vuelosItinerarios.itinerariosCambiosPar(a1, a2))
-          case "Itinerario Salida" => campo.setText("" + vuelosItinerarios.itenerariosSalidaPar(a1, a2, hora, min))
+          case "Itinerario Salida" => campo.setText("" + vuelosItinerarios.itinerariosSalidaPar(a1, a2, hora, min))
         }
 
       case "Secuencial" =>
         accion match {
-          case "Itinerarios" => campo.setText("" + vuelosItinerarios.itenerario(a1, a2))
+          case "Itinerarios" => campo.setText("" + vuelosItinerarios.itinerario(a1, a2))
           case "Itinerarios Tiempo Vuelo" => campo.setText("" + vuelosItinerarios.itinerariosTiempo(a1, a2))
-          case "Itinerarios Distancia" => campo.setText("" + vuelosItinerarios.itenerarioDistancia(a1, a2))
+          case "Itinerarios Distancia" => campo.setText("" + vuelosItinerarios.itinerarioDistancia(a1, a2))
           case "itinerarios escala" => campo.setText("" + vuelosItinerarios.itinerariosCambios(a1, a2))
-          case "Itinerario Salida" => campo.setText("" + vuelosItinerarios.itenerariosSalida(a1, a2, hora, min))
+          case "Itinerario Salida" => campo.setText("" + vuelosItinerarios.itinerariosSalida(a1, a2, hora, min))
         }
       case "" =>
     }
